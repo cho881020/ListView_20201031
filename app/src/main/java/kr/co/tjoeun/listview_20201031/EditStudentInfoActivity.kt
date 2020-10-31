11b6cc370258c8700c525d28111a3c3508475b7e
조경진 <kj_cho@idealidea.co.kr>
2020-10-31 15:13:32 +0900

    학생 정보 입력화면 추가

---
+++ app/src/main/java/kr/co/tjoeun/listview_20201031/EditStudentInfoActivity.kt
@@ -0,0 +1,11 @@
+package kr.co.tjoeun.listview_20201031
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditStudentInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_student_info)
+    }
+}
